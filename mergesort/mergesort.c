1951aba44280e10675767a84046a40da14c0d045
JReuvers <reuve026@morris.umn.edu>
2020-09-25 17:17:11 -0500

    Fix memory leak errors

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -9,12 +9,13 @@ bool needsSorting(int rangeSize) {
         }
 }
 
-void mergeRanges(int toSort[], int startIndex, int midPoint, int endIndex) {
+void mergeRanges(int* toSort, int size, int startIndex, int midPoint, int endIndex) {
 	int rangeSize = endIndex - startIndex;
-	int *destination = (int*) calloc(rangeSize+ + 1, sizeof(int));
+	int* destination = (int*) calloc((size + 1), sizeof(int));
 	int firstIndex = startIndex;
 	int secondIndex = midPoint;
 	int copyIndex = 0;
+
 	while (firstIndex < midPoint && secondIndex < endIndex) {
 		if (toSort[firstIndex] < toSort[secondIndex]) {
 			destination[copyIndex] = toSort[firstIndex];
@@ -25,32 +26,37 @@ void mergeRanges(int toSort[], int startIndex, int midPoint, int endIndex) {
 		}
 		copyIndex++;
 	}
+
 	while (firstIndex < midPoint) {
 		destination[copyIndex] = toSort[firstIndex];
 		copyIndex++;
 		firstIndex++;
 	}
+
 	while (secondIndex < endIndex) {
 		destination[copyIndex] = toSort[secondIndex];
 		copyIndex++;
-		firstIndex++;
+		secondIndex++;
 	}
+
 	for (int i = 0; i < rangeSize; i++) {
 		toSort[i + startIndex] = destination[i];
 	}
+
 	free(destination);
 }
 
-void mergeSortRange(int toSort[], int startIndex, int endIndex) {
+void mergeSortRange(int* toSort, int startIndex, int endIndex) {
 	int rangeSize = endIndex - startIndex;
-        if (needsSorting(rangeSize)) {
+        
+	if (needsSorting(rangeSize)) {
 		int midPoint = (startIndex + endIndex) / 2;
                 mergeSortRange(toSort, startIndex, midPoint);
                 mergeSortRange(toSort, midPoint, endIndex);
-                mergeRanges(toSort, startIndex, midPoint, endIndex);
+                mergeRanges(toSort, rangeSize, startIndex, midPoint, endIndex);
         }
 }
 
-void mergesort(int size, int toSort[]) {
+void mergesort(int size, int* toSort) {
 	  mergeSortRange(toSort, 0, size);
 }
